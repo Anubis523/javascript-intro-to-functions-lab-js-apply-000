68523fa9dd7622640171711b8b5068cd0c08df3c
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-17 00:35:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  return = (string.toLowerCase() === string)
-  ? "I can't hear you!"
-  : "YES INDEED";
+  if(string === "I love you, Grandma") {
+    return "I love you, too."
+  }
+  else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
 }
